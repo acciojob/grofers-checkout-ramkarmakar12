19240f3fa960bebe24e7f6ee0431d643d04fa254
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-16 11:15:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let sum=0;
 
 const getSum = () => {
 //Add your code here
+	let sum = 0;
+  let prices = document.getElementsByClassName("price");
+  for (let price of prices) {
+    sum += Number(price.innerHTML);
+  }
+	
+
+  const newRow = document.querySelector("table").insertRow(-1);
+	  newRow.setAttribute("id","ans");
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  cell1.innerText = "Total Value";
+  cell2.innerText = sum;
+	
+	
   
 };
 
 getSumBtn.addEventListener("click", getSum);
-
